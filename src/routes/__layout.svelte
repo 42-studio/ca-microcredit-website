c7f3abb3d670b293621c504167c4a5f4cae19659
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2022-07-11 16:59:05 +0100

    Completed title page (home) + nav + footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,63 @@
+<script>
+  import { page } from "$app/stores";
+</script>
+
 <nav>
-  <img
-    src="/images/logo.png"
-    alt="C.A. Micro Credit Enterprise Logo"
-    class="logo"
-  />
+  <a href="/">
+    <img
+      src="/images/logo.png"
+      alt="C.A. Micro Credit Enterprise Logo"
+      class="logo-nav"
+    />
+  </a>
   <div class="links">
-    <a href="/">Home</a>
-    <a href="/stories">Stories</a>
-    <a href="/contact">Contact</a>
+    <a href="/" style="opacity: {$page.url.pathname == '/' ? 1 : 0.5}">Home</a>
+    <a
+      href="/stories"
+      style="opacity: {$page.url.pathname == '/stories' ? 1 : 0.5}">Stories</a
+    >
+    <a
+      href="/contact"
+      style="opacity: {$page.url.pathname == '/contact' ? 1 : 0.5}">Contact</a
+    >
   </div>
 </nav>
 
 <slot />
+
+<footer>
+  <section class="top">
+    <section class="left">
+      <p class="footer-title">C.A. Mirco Credit<br />Enterprise</p>
+      <p class="footer-paragraph">Some rights reserved</p>
+    </section>
+    <section class="right">
+      <img
+        src="/images/logo.png"
+        alt="C.A. Micro Credit Enterprise Logo"
+        class="logo-footer"
+      />
+    </section>
+  </section>
+  <hr />
+  <section class="bottom">
+    <section class="left">
+      <p class="footer-paragraph">© {new Date().getFullYear()}</p>
+    </section>
+    <section class="right">
+      <p class="footer-paragraph">
+        website by <a
+          class="footer-a"
+          href="https://joshlucpoll.dev"
+          target="_blank"
+          rel="noopener noreferrer">joshlucpoll</a
+        ><br /><a class="footer-a" target="_blank" href="https://icons8.com"
+          >icons by Icons8</a
+        >
+      </p>
+    </section>
+  </section>
+</footer>
 
 <style>
   nav {
@@ -21,25 +67,85 @@
     align-items: center;
   }
 
-  .links > * {
-    margin-left: 20px;
-    color: var(--black);
-  }
-
   .links {
     display: block;
     text-align: right;
     line-height: 2rem;
     font-size: 16px;
   }
+  .links > * {
+    margin-left: 20px;
+    text-decoration: none;
+    color: var(--black);
+    border-bottom: 1px solid var(--secondary);
+    padding-bottom: 1px;
 
-  @media screen and (max-width: 1024px) {
+    transition: all 200ms ease;
+  }
+
+  .links > *:hover {
+    padding-bottom: 4px;
+  }
+
+  footer {
+    padding: 20px;
+    font-size: 12px;
+    color: var(--black);
+  }
+
+  section.top {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  section.bottom {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    opacity: 0.5;
+    line-height: 1.3rem;
+  }
+
+  .footer-a {
+    text-decoration: underline;
+    color: var(--black);
+  }
+
+  section.right {
+    text-align: right;
+  }
+  .footer-title {
+    font-weight: 700;
+  }
+
+  hr {
+    height: 1px;
+    background-color: var(--secondary);
+    opacity: 0.5;
+    border: none;
+  }
+
+  @media screen and (max-width: 600px) {
     nav {
       padding: 28px;
     }
-    .logo {
+    .logo-nav {
       height: 45px;
-      widows: 45px;
+    }
+    .logo-footer {
+      height: 60px;
+    }
+  }
+
+  @media screen and (min-width: 600px) {
+    nav {
+      padding: 28px;
+    }
+    .logo-nav {
+      height: 85px;
+    }
+    .logo-footer {
+      height: 100px;
     }
   }
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,79 @@
   <title>Home • C.A. Micro Credit Enterprise</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<section class="title">
+  <h1>C.A. Micro Credit</h1>
+  <h2>supporting local<br />communities<br />in Ghana</h2>
+
+  <img src="/images/leaves.svg" alt="leaves" class="leaves" />
+  <div class="learn-more-container">
+    <img src="/images/man.png" alt="man" class="man" />
+    <button>Learn more</button>
+  </div>
+</section>
+<section class="mission" />
+
+<style>
+  section.title {
+    padding: 28px;
+    padding-top: 0;
+    min-height: calc(100vh - 200px);
+  }
+
+  h1,
+  h2 {
+    display: block;
+    text-align: right;
+    font-family: "Roboto Slab", serif;
+    color: var(--black);
+    font-size: 30px;
+    margin: 0;
+    line-height: 3rem;
+  }
+
+  h1 {
+    font-weight: 700;
+  }
+  h2 {
+    font-weight: 400;
+  }
+
+  .leaves {
+    position: absolute;
+    left: 0;
+    top: 250px;
+    z-index: -1;
+  }
+
+  .learn-more-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: calc(100vh - 730px);
+  }
+  .man {
+    width: 230px;
+    /* max-width: 40vh; */
+  }
+
+  button {
+    padding: 10px 70px;
+    margin: 0;
+    border-radius: 10px;
+    border: none;
+    background-color: var(--primary);
+    box-shadow: 0px 3px 15px 6px rgba(0, 0, 0, 0.1);
+
+    color: var(--white);
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 0.5px;
+  }
+
+  section.mission {
+    background-color: var(--white);
+    height: 100px;
+    border-radius: 20px 20px 0px 0px;
+    box-shadow: 0px 0px 50px rgba(0, 0, 0, 0.1);
+  }
+</style>
