8f91773d8c01c7c75d4fd922626307e8ba7b0448
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2023-01-09 01:04:15 +0000

    new build

--- docs/_app/immutable/pages/invest.svelte-82ff2f50.js
+++
@@ -1,1 +0,0 @@
-import{S as n,i as a,s as o,X as r,d,n as e}from"../chunks/index-8ccbaca9.js";function c(s){return{c(){this.h()},l(t){r('[data-svelte="svelte-151pbye"]',document.head).forEach(d),this.h()},h(){document.title="Invest \u2022 C.A. Micro Credit Enterprise"},m:e,p:e,i:e,o:e,d:e}}class h extends n{constructor(t){super(),a(this,t,null,c,o,{})}}export{h as default};
