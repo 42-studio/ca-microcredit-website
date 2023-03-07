acb3df584d20889f4b5699f6958d5dbfc3028d14
Josh Pollard <49789046+Joshlucpoll@users.noreply.github.com>
2023-03-07 10:45:28 +0000

    new build

---
+++ docs/_app/immutable/pages/stories/karim-zenabu.svelte-da7ba80e.js
@@ -0,0 +1,69 @@
+import{S as O,i as Q,s as T,e as t,t as u,k as f,c as r,a as h,h as p,d as i,m,b as n,U as W,g as X,I as e,n as D}from"../../chunks/index-8ccbaca9.js";function Y(L){let s,l,_,G,c,F,x,a,k,A,C,b,H,Z,y,E,M,v,I,K,w,q,j,g,P;return{c(){s=t("div"),l=t("a"),_=u("Back to stories"),G=f(),c=t("img"),x=f(),a=t("div"),k=t("h1"),A=u("Karim Zenabu"),C=f(),b=t("p"),H=u(`Did you know that most women in polygamous marriages in Ghana are
+      responsible for taking care of their own children?`),Z=f(),y=t("p"),E=u(`Meet Karim Zenabu, a busy mother of four children who lives in Garu in the
+      Upper East region of Ghana. She is married with two co-wives. Ramatu is in
+      the soya bean cake business and has been in this business for the past
+      five years. She said her business was successful until the prices of goods
+      and services went up, and she made little profit from the business due to
+      inadequate working capital. As a polygamous married woman, it is her
+      responsibility to take care of her children's school fees, hospital bills,
+      and feed and clothe them. Zenabu said she applied for a loan from a formal
+      bank to expand her business and increase her income level, but her loan
+      request was not granted because she could not provide
+      collateral/guarantors for the loan. She became worried about the future
+      and almost lost hope. According to her, she could no longer carry out her
+      family's responsibilities, which affected her relationship with her
+      children.`),M=f(),v=t("p"),I=u(`C.A. Micro-Credit came along the line and supported her with a loan of
+      GHS1,000. She used GHS500 to buy soya beans, GHS245 to buy 12.5 liters of
+      cooking oil, GHS200 to buy firewood, and GHS55 for other ingredients for
+      her business. For a period of six months, she repaid her loan and had a
+      net profit of GHS600 extra income compared to previously. She said the
+      extra income was used to buy food, pay her children's school fees, and
+      partly invest in her business.`),K=f(),w=t("p"),q=u(`Zenabu is on her second loan cycle and doing very well in her soya bean
+      cake business. She said her fears and worries about the future are a thing
+      of the past. She said her children are now happy with her and call her
+      "sweet mummy." She expressed how joyful she feels each time her children
+      call her "sweet mummy."`),j=f(),g=t("p"),P=u(`She plans to build a soya bean cake factory in the future and employ and
+      train young girls who have dropped out of school to have a hand in work
+      and work their way out of poverty and be financially independent. She said
+      she has not forgotten her dream to build a house for herself and her
+      children so they can have a comfortable and peaceful home. Zenabu advises
+      her colleagues to focus on reinvesting their profits in their businesses
+      instead of using it to buy expensive clothes and jewelry. She believes
+      that a woman's freedom and dignity depend on her level of income. Finally,
+      she said, "I am most grateful to the Almighty God and C.A. Micro-Credit
+      for restoring hope to me and my children. God bless C.A. Micro-Credit's
+      investors."`),this.h()},l(S){s=r(S,"DIV",{class:!0});var d=h(s);l=r(d,"A",{class:!0,href:!0,"sveltekit:noscroll":!0});var U=h(l);_=p(U,"Back to stories"),U.forEach(i),G=m(d),c=r(d,"IMG",{src:!0,alt:!0,class:!0}),x=m(d),a=r(d,"DIV",{class:!0});var o=h(a);k=r(o,"H1",{});var B=h(k);A=p(B,"Karim Zenabu"),B.forEach(i),C=m(o),b=r(o,"P",{class:!0});var R=h(b);H=p(R,`Did you know that most women in polygamous marriages in Ghana are
+      responsible for taking care of their own children?`),R.forEach(i),Z=m(o),y=r(o,"P",{class:!0});var V=h(y);E=p(V,`Meet Karim Zenabu, a busy mother of four children who lives in Garu in the
+      Upper East region of Ghana. She is married with two co-wives. Ramatu is in
+      the soya bean cake business and has been in this business for the past
+      five years. She said her business was successful until the prices of goods
+      and services went up, and she made little profit from the business due to
+      inadequate working capital. As a polygamous married woman, it is her
+      responsibility to take care of her children's school fees, hospital bills,
+      and feed and clothe them. Zenabu said she applied for a loan from a formal
+      bank to expand her business and increase her income level, but her loan
+      request was not granted because she could not provide
+      collateral/guarantors for the loan. She became worried about the future
+      and almost lost hope. According to her, she could no longer carry out her
+      family's responsibilities, which affected her relationship with her
+      children.`),V.forEach(i),M=m(o),v=r(o,"P",{class:!0});var z=h(v);I=p(z,`C.A. Micro-Credit came along the line and supported her with a loan of
+      GHS1,000. She used GHS500 to buy soya beans, GHS245 to buy 12.5 liters of
+      cooking oil, GHS200 to buy firewood, and GHS55 for other ingredients for
+      her business. For a period of six months, she repaid her loan and had a
+      net profit of GHS600 extra income compared to previously. She said the
+      extra income was used to buy food, pay her children's school fees, and
+      partly invest in her business.`),z.forEach(i),K=m(o),w=r(o,"P",{class:!0});var N=h(w);q=p(N,`Zenabu is on her second loan cycle and doing very well in her soya bean
+      cake business. She said her fears and worries about the future are a thing
+      of the past. She said her children are now happy with her and call her
+      "sweet mummy." She expressed how joyful she feels each time her children
+      call her "sweet mummy."`),N.forEach(i),j=m(o),g=r(o,"P",{class:!0});var J=h(g);P=p(J,`She plans to build a soya bean cake factory in the future and employ and
+      train young girls who have dropped out of school to have a hand in work
+      and work their way out of poverty and be financially independent. She said
+      she has not forgotten her dream to build a house for herself and her
+      children so they can have a comfortable and peaceful home. Zenabu advises
+      her colleagues to focus on reinvesting their profits in their businesses
+      instead of using it to buy expensive clothes and jewelry. She believes
+      that a woman's freedom and dignity depend on her level of income. Finally,
+      she said, "I am most grateful to the Almighty God and C.A. Micro-Credit
+      for restoring hope to me and my children. God bless C.A. Micro-Credit's
+      investors."`),J.forEach(i),o.forEach(i),d.forEach(i),this.h()},h(){n(l,"class","back svelte-1lbnokh"),n(l,"href","/stories"),n(l,"sveltekit:noscroll",""),W(c.src,F="/images/stories/KARIM_ZENABU.jpg")||n(c,"src",F),n(c,"alt","Karim Zenabu"),n(c,"class","svelte-1lbnokh"),n(b,"class","svelte-1lbnokh"),n(y,"class","svelte-1lbnokh"),n(v,"class","svelte-1lbnokh"),n(w,"class","svelte-1lbnokh"),n(g,"class","svelte-1lbnokh"),n(a,"class","story-wrapper svelte-1lbnokh"),n(s,"class","wrapper svelte-1lbnokh")},m(S,d){X(S,s,d),e(s,l),e(l,_),e(s,G),e(s,c),e(s,x),e(s,a),e(a,k),e(k,A),e(a,C),e(a,b),e(b,H),e(a,Z),e(a,y),e(y,E),e(a,M),e(a,v),e(v,I),e(a,K),e(a,w),e(w,q),e(a,j),e(a,g),e(g,P)},p:D,i:D,o:D,d(S){S&&i(s)}}}class ee extends O{constructor(s){super(),Q(this,s,null,Y,T,{})}}export{ee as default};
